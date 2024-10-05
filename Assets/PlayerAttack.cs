f628418bc12b07f190f83bcd00d7392b55c026c4
Katelyn P <kprizlee@comcast.net>
2024-10-05 14:49:48 -0400

    Update Player Attack

---
+++ Assets/PlayerAttack.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+
+    [SerializeField] private Animator anim;
+
+    [SerializeField] private float meleeSpeed;
+
+    [SerializeField] private float damage;
+
+    float timeUntilMelee; 
+
+    private void Update(){  
+
+        if(timeUntilMelee <= 0f ) {
+            if(Input.GetMouseButtonDown(0)){
+                anim.SetTrigger("Attack");
+                timeUntilMelee = meleeSpeed;
+            }
+        }else{
+            timeUntilMelee -= Time.deltaTime;
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other){
+
+        if(other.tag == "Enemy"){
+            //other.GetComponent<Enemy>().TakeDamage(damage);
+            Debug.Log("Enemy hit");
+
+        }
+
+    }
+
+
+
+}
